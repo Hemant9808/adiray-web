225467823275f523c981ebc94ca6753061ed10e5
Hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2024-04-19 00:28:06 +0530

    responsive navbara

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,10 +24,10 @@ const Navbar = () => {
 
     return (
         <>
-        <div className="z-50  w-full h-[120px] bg-white  fixed flex items-center justify-center shadow-2xl">
+        <div className="z-50  w-full h-[120px] bg-[#dae8ea]  fixed flex items-center justify-center shadow-2xl">
             <div className="w-full h-[120px] flex items-center justify-center " >
-                 <img className="h-[100px] w-[200px] " src={Logo} alt="adiray" />
-            </div>
+                {!open && ( <img className="h-[100px] w-[200px] " src={Logo} alt="adiray" />
+          ) } </div>
         </div>
         <motion.nav
         variants={{
@@ -38,13 +38,13 @@ const Navbar = () => {
           animate={hidden ? "hidden" : "visible"}
           transition={{ duration:0.35, ease :"easeInOut"}}
         
-        className = { `  z-50 fixed w-full h-[153px] bg-[#dae8ea] px-4 border-b-2 flex flex-col justify-center items-center shadow-2xl `}>
-             <div className=" px-5 py-2 w-[80%]  bg-white rounded-xl justify-between items-center "  >
-             <menu className=" flex items-center justify-between object-cover " style={{width:"100%"}} >
+        className = { `  z-50 fixed w-full h-[120px]  bg-[#dae8ea] px-4 border-b-2 flex flex-col justify-center items-center  `}>
+             <div className=" px-5 py-[2%] w-[100%] md:w-[80%] sm:w-full  bg-white rounded-xl justify-between items-center "  >
+             <menu className="  hidden md:block items-center justify-between object-cover " style={{width:"100%"}} >
                    
-                <ul className=" hidden items-center w-full md:flex " style={{paddingTop:"30px",paddingBottom:"30px", display:"flex",  flexDirection:'row', justifyContent:"space-around"}}>
+               <ul className=" items-center w-full  " style={{display:"flex",  flexDirection:'row', justifyContent:"space-around"}}>
                     <li >
-                        <NavLink className={({ isActive }) => isActive ? "text-blue-800" : "text-black"} to="/">Home</NavLink>
+                        <NavLink className={({ isActive }) => isActive ? "text-blue-800" : "text-black"}  to="/">Home</NavLink>
                     </li>
                     <li>
                         <NavLink className={({ isActive }) => isActive ? "text-blue-800" : "text-black"} to="chatbot">AI Chatbot</NavLink>
@@ -64,14 +64,18 @@ const Navbar = () => {
                     </li>
                 </ul>
                
-                <button className="inline-flex md:hidden" onClick={() => setOpen(!open)}>
-                    {open ? <AiOutlineClose /> : <FiMenu />}
-                </button>
+                
             </menu>
+            <button className=" md:hidden flex items-center justify-center"   onClick={() => setOpen(!open)}>
+                    {open ? <AiOutlineClose  /> : <FiMenu style={{fontSize:"25px"}} />}
+                </button>
             </div>
 
             {open && (
-                <ul className="p-6 flex flex-col gap-4">
+                <ul className="p-6 md:hidden  flex-col gap-8 bg-[#dae8ea] w-[50%] h-[800px]  rounded-md " style={{marginTop:"475px", marginLeft:"60%"}}>
+                    <button className=" md:hidden flex items-center justify-end "   onClick={() => setOpen(!open)}>
+                    {open ? <AiOutlineClose  style={{ fontSize: '30px', }}   /> : <FiMenu />}
+                </button>
                     <li>
                         <NavLink className={({ isActive }) => isActive ? "text-blue-800" : "text-black"} to="/">Home</NavLink>
                     </li>
