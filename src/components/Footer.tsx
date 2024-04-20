4006958ba18d0a742a98b378ac8804890ef7a2ef
Hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2024-04-20 23:56:35 +0530

    abs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,82 +1,81 @@
 import styles from "../styles/footer.module.css"
 import { Link } from "react-router-dom";
-import Logo from "../assets/Logo.png"
+import silverlogo from "../assets/logo/silverlogo.png"
 import { AiFillInstagram } from "react-icons/ai";
 import { BsFacebook, BsTwitterX, BsYoutube, BsLinkedin } from "react-icons/bs";
 
 
 const Footer = () => {
     return (
-        <footer className={styles.wrapper}>
+        <footer className={styles.wrapper} style={{ backgroundColor:"#0B1321",margin:"0"}}>
             <div className={styles.list}>
-                <div className="flex flex-col gap-3 ">
-                    <img className="w-[150px] h-[100px]  justify-center " src={Logo} alt="adiray" />
-                    <p className="text-[#9A9A9A]">
-                        Address any issue effortlessly with a simple
-                        question. Problem-solving simplified
-                    </p>
+                <div className="flex flex-col gap-3">
+                    <img className="w-[90px]  justify-center " style={{marginLeft:"-10px"}} src={silverlogo} alt="adiray" />
+                
                     <div className="flex gap-5 items-center ">
-                        <Link to={"https://www.linkedin.com/company/adiray-global"}>
-                            <BsLinkedin />
+                        <Link className=" " to={"https://www.linkedin.com/company/adiray-global"}>
+                             <BsLinkedin className="w-8 h-8 "/>
                         </Link>
                    
                         <Link to={"https://twitter.com/AdirayGlobal"}>
-                            <BsTwitterX />
+                            <BsTwitterX  className="w-8 h-8 "/>
                         </Link>
 
                         <Link to={"https://www.facebook.com/share/xDBzdbxqt3TijffV/?mibextid=WC7FNe"}>
-                            <BsFacebook />
+                            <BsFacebook className="w-8 h-8 " />
                         </Link>
 
                         <Link to={"https://www.instagram.com/adirayglobal/"}>
-                            <AiFillInstagram />
+                            <AiFillInstagram className="w-8 h-8 " />
                         </Link>
 
                         <Link to={" https://www.youtube.com/@ADIRAYGLOBAL"}>
-                            <BsYoutube />
+                            <BsYoutube className="w-8 h-8 " />
                         </Link>
 
                     </div>
                 </div>
 
                 <ul className="md:ml-[50px] ">
-                    <h2 className="text-lg font-semibold mb-2 ">Quick Links</h2>
-                    <li>
-                        <Link to="">
+                    <h2 className="text-lg font-semibold mb-2 text-white ">Quick Links</h2>
+                   
+                    <li >
+                        <Link className="text-gray-300" to="">
                             Products
                         </Link>
                     </li>
                     <li>
-                        <Link to="">
+                        <Link className="text-gray-300" to="">
                             Contact us
                         </Link>
                     </li>
                     <li>
-                        <Link to="">
+                        <Link className="text-gray-300" to="">
                             terms & conditions
                         </Link>
                     </li>
                     <li>
-                        <Link to="">
+                        <Link className="text-gray-300" to="">
                             privacy and cookies
                         </Link>
                     </li>
                     <li>
-                        <Link to="">
+                        <Link className="text-gray-300"  to="">
                             licenses
                         </Link>
                     </li>
+                   
                 </ul>
-
+                
                 <ul>
-                    <h2 className="text-lg font-semibold mb-2">Contact</h2>
-                    <Link className="text-wrap" to="https://maps.app.goo.gl/SykjUggdHa4SkLYbA">
+                    <h2 className="text-lg text-white font-semibold mb-2">Contact</h2>
+                    <Link className="text-wrap text-gray-300" to="https://maps.app.goo.gl/SykjUggdHa4SkLYbA">
                         Address : D 1807, Shriram Greenfield, Bommenahalli, Bangalore. 560049
                     </Link>
-                    <Link to="mailto:admin@adirayglobal.com">
+                    <Link className="text-gray-300"  to="mailto:admin@adirayglobal.com">
                         EmailId : admin@adirayglobal.com
                     </Link>
-                    <Link to="tel:+919620199884">
+                    <Link className="text-gray-300" to="tel:+919620199884">
                         Mobile : 9620199884
                     </Link>
                 </ul>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import contact from "../assets/contact.png";
-import mail from "../assets/logo/mail.svg"
-import phone from "../assets/logo/phone.svg"
-import gps from "../assets/logo/gps.svg"
+import mail from "../assets/logo/mail.svg";
+import phone from "../assets/logo/phone.svg";
+import gps from "../assets/logo/gps.svg";
 import { BiPhoneOutgoing } from "react-icons/bi";
 export default function Contact() {
+  const [sentMsg, setSentMsg] = useState(false);
   return (
     <div className="w-[100%] h-auto p-[75px] bg-gradient-to-br from-blue-100 to-amber-50 flex-col justify-center items-center gap-2.5 inline-flex mt-[100px]">
       <div className="justify-center items-center gap-[30px] flex md:flex-col flex-col lg:flex-row ">
         <div
           className="p-[30px] w-[500px] h-[820px] bg-white  rounded-3xl shadow flex flex-col justify-center items-center "
           style={{ maxWidth: "90%" }}
-        > <div class="w-[241px] text-center"><span class="text-gray-900 text-3xl font-semibold ">Message</span><span class="text-black text-3xl font-semibold "> </span><span class="text-blue-900 text-3xl font-semi bold ">Sent.</span></div>
-        <div class="w-[431px] text-center text-gray-900 text-xl font-normal mt-2 ">We will connect with you shortly</div>
-         </div>
+        >
+          {!sentMsg && (<>
+            <div className="w-[100%] h-[100%] pt-4  rounded-3xl shadow justify-start items-start inline-flex">
+              <div className="p-5 justify-center items-center gap-2.5 flex">
+                <div className="flex-col  justify-center items-center gap-[35px] inline-flex">
+                  <div className="flex-col justify-center items-center gap-[30px] flex">
+                    <div className="flex-col justify-start items-start gap-3 flex">
+                      <div className="flex-col justify-start items-start flex">
+                        <div className="text-pink-600 text-lg font-extrabold ">
+                          Get in Touch
+                        </div>
+                        <div className="text-center">
+                          <span className="text-gray-900 text-5xl font-bold ">
+                            Reach Out{" "}
+                          </span>
+                          <span className="text-blue-900 text-5xl font-bold ">
+                            to
+                          </span>
+                          <span className="text-gray-900 text-5xl font-extrabold ">
+                            {" "}
+                          </span>
+                          <span className="text-blue-900 text-5xl font-bold ">
+                            Us
+                          </span>
+                        </div>
+                      </div>
+                      <h3 className="w-[100%] h-12 whitespace-normal word-wrap text-slate-500 text-md ">
+                        Address any issue effortlessly with a simple question.
+                        Problem-solving simplified.
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="flex-col w-[100%] justify-start items-start gap-[30px] flex">
+                    <div className=" w-[100%] flex-col justify-start items-start gap-1.5 flex">
+                      <div className="flex-col justify-start items-start gap-1.5 flex">
+                        <div className="text-slate-700  text-sm font-bold ">
+                          Name
+                        </div>
+                      </div>
+                      <div className="w-[100%]  h-11   bg-white rounded-md border border-neutral-300 justify-start items-center gap-2.5 inline-flex">
+                        <input className="w-[100%] h-[100%] pl-5 text-gray-400 text-[12px] font-semibold " placeholder="Name">
+                          
+                        </input>
+                      </div>
+                    </div>
+                    <div className=" w-[100%] flex-col justify-start items-start gap-1.5 flex">
+                      <div className="text-slate-600 text-sm font-bold ">
+                        Contact{" "}
+                      </div>
+                      <div className="w-[100%]  h-11   bg-white rounded-md border border-neutral-300 justify-start items-center gap-2.5 inline-flex">
+                        <input className="w-[100%] h-[100%] pl-5 text-gray-400 text-[12px] font-semibold " placeholder="phone number/email address">
+                          
+                        </input>
+                      </div>
+                    </div>
+                    <div className=" w-[100%]  flex-col justify-start items-start gap-1.5 flex">
+                      <div className="text-slate-600 text-sm font-bold ">
+                        Your Message
+                      </div>
+                      <div className="w-[100%] h-56   bg-white rounded-lg border border-neutral-300 justify-start items-start gap-2.5 inline-flex">
+                        <div className="w-[100%] h-56  justify-start items-start gap-2.5 flex">
+                          <textarea  className="text-gray-400 p-5 w-[100%] h-[100%] text-[13px] font-semibold  rounded-lg border-none">
+                            Leave Us Your Message...
+                          </textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-[100%] h-11 py-2.5 bg-blue-900 rounded-md justify-center items-center gap-2.5 inline-flex">
+                    <div className="text-slate-200 text-sm font-bold ">
+                      Send Message
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>)}
+
+          {sentMsg && (
+            <>
+              <div className="w-[241px] text-center">
+                <span className="text-gray-900 text-3xl font-semibold ">
+                  Message
+                </span>
+                <span className="text-black text-3xl font-semibold "> </span>
+                <span className="text-blue-900 text-3xl font-semi bold ">
+                  Sent.
+                </span>
+              </div>
+              <div className="w-[431px] text-center text-gray-900 text-xl font-normal mt-2 ">
+                We will connect with you shortly
+              </div>
+            </>
+          )}
+        </div>
         <div className="flex-col  gap-[30px] inline-flex justify-center items-center">
           <div className="rounded-3xl shadow flex-col justify-center items-center gap-25 flex">
             <img
@@ -26,17 +119,16 @@ export default function Contact() {
               <div className="h-[78px] px-6 py-5 bg-neutral-100 rounded-xl flex-col justify-start items-start gap-2.5 flex">
                 <div className="justify-start items-center gap-[23px] inline-flex">
                   <div className="w-8 h-8 relative">
-                 
                     <img
                       className=" md:w-[100%] w-[80%]  h-[80%] rounded-3xl"
                       src={mail}
                     />
                   </div>
                   <div className="flex-col justify-start items-start inline-flex">
-                    <div className="text-slate-700 text-lg font-bold font-['Mont']">
+                    <div className="text-slate-700 text-lg font-bold ">
                       Email
                     </div>
-                    <div className="w-[252px] text-slate-600 text-xs font-semibold font-['Mont']">
+                    <div className="w-[252px] text-slate-600 text-xs font-semibold ">
                       contact@adirayglobal.com
                     </div>
                   </div>
@@ -44,15 +136,17 @@ export default function Contact() {
               </div>
               <div className="h-[78px] px-6 py-5 bg-neutral-100 rounded-xl flex-col justify-start items-start gap-2.5 flex">
                 <div className="justify-start items-center gap-[23px] inline-flex">
-                  <div className="w-8 h-8 relative"><img
+                  <div className="w-8 h-8 relative">
+                    <img
                       className=" md:w-[100%] w-[80%]  h-[80%] rounded-3xl"
                       src={phone}
-                    /></div>
+                    />
+                  </div>
                   <div className="flex-col justify-start items-start inline-flex">
-                    <div className="text-slate-700 text-lg font-bold font-['Mont']">
+                    <div className="text-slate-700 text-lg font-bold ">
                       Phone
                     </div>
-                    <div className="w-[252px] text-slate-600 text-xs font-semibold font-['Mont']">
+                    <div className="w-[252px] text-slate-600 text-xs font-semibold ">
                       9620199884 | 9525500039
                     </div>
                   </div>
@@ -60,15 +154,17 @@ export default function Contact() {
               </div>
               <div className="h-[78px] px-6 py-5 bg-neutral-100 rounded-xl flex-col justify-start items-start gap-2.5 flex">
                 <div className="w-[100%] justify-start items-center gap-[23px] inline-flex">
-                  <div className="w-8 h-8 relative"><img
+                  <div className="w-8 h-8 relative">
+                    <img
                       className=" md:w-[100%] w-[80%]  h-[80%] rounded-3xl"
                       src={gps}
-                    /></div>
+                    />
+                  </div>
                   <div className="flex-col justify-start items-start inline-flex">
-                    <div className="text-slate-700 text-lg font-bold font-['Mont']">
+                    <div className="text-slate-700 text-lg font-bold ">
                       Address
                     </div>
-                    <div className="w-[100%] text-slate-600 text-xs font-semibold font-['Mont']">
+                    <div className="w-[100%] text-slate-600 text-xs font-semibold ">
                       D 1807, Shriram Greenfield, Bommenahalli, Bangalore 560049
                     </div>
                   </div>
