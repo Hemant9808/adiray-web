e1afe067020318a9a1b575ad5a045794614e30a5
Hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2024-04-21 22:49:05 +0530

    chatbot page add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import Chatbot from "./pages/AI chatbot/Chatbot";
 const Enquiry = lazy(() => import("./pages/Home/Enquiry"));
 const Signup = lazy(() => import("./pages/Home/Signup"));
 const Login = lazy(() => import("./pages/Login"));
 const Layout = lazy(() => import("./pages/Home/Layout"));
 const Home = lazy(() => import("./pages/Home/Home"));
-const Layouts = lazy(() => import("./pages/AI chatbot/Layout"));
+const Layouts = lazy(() => import("./pages/AI chatbot/Chatbot"));
 const Contact = lazy(() => import("./pages/Contact"));
 
 
@@ -41,7 +42,7 @@ function App() {
     },
     {
       path: "/chatbot",
-      element: <Layouts />,
+      element: <Chatbot />,
       children: [
       ]
     }
