bba152ffd963eb6ea37bb4c5e43aaac48d5592b3
Hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2024-04-24 17:13:43 +0530

    none

--- src/pages/Home/Blog.tsx
+++ src/pages/Home/Blog.tsx
@@ -4,9 +4,9 @@ import { Button } from "../../components/Button";
 import blogpage from "../../assets/blogpage.png"
 export default function Blog() {
   return (
-    <div className="h-[100vh] w-full relative top-[20vh] mb-5" >
-      <div className="flex justify-center items-center px-5 "><span className="text-gray-900 text-5xl font-extrabold ">Latest </span><span className="text-blue-900 text-5xl font-extrabold ml-3">Updates</span><span className="text-gray-900 text-5xl font-extrabold "> </span></div>
-      <section className=" flex justify-center items-center py-20 px-5 ">
+    <div className="h-full w-full relative top-[7vh] bottom-[0vh] flex flex-col items-center " >
+      <div className=" absolute w-full flex  justify-center  items-center top-[15vh] "><span className="text-gray-900 text-5xl font-bold ">Latest </span><span className="text-blue-900 text-5xl font-bold ml-3">Updates</span><span className="text-gray-900 text-5xl font-extrabold "> </span></div>
+      <section className=" flex justify-center items-center  py-60 px-5  ">
         <div className="flex flex-wrap gap-8 justify-center">
           <div className="w-[390px] h-[390px] rounded-md shadow border border-gray-900 flex-col justify-center items-center inline-flex">
             <div className=" bg-sky-950 bg-opacity-0 rounded-md z-1"><img className="z-1 w-[390px] h-[390px]" src={blogpage} alt="" /></div>
@@ -41,7 +41,7 @@ export default function Blog() {
                 Unlocking New Markets
               </div>
               <div className="z-3 relative self-stretch text-slate-600 text-xs font-semibold ">
-                Discover innovative strategies for exporting unique goods
+                Discover innovative strategies for exporting unique goods   
                 globally. From market research to branding, unlock success in
                 non-traditional commodity trade.
               </div>
