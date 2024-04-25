f82577c603ea5e2f67d2c88b65daf5dd5ec2e44b
Hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2024-04-25 19:18:32 +0530

    none

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -55,7 +55,7 @@ export default function Products() {
 
   return (
     <div className="w-full h-auto flex flex-col justify-center items-center mb-4 ">
-        <div className="relative h-64 overflow-hidden group mb-24 mt-[100px]">
+        <div className="relative h-64 w-full overflow-hidden group mb-24 mt-[100px]">
                 <img className="object-cover h-full w-full group-hover:scale-150 ease-in-out duration-1000" src={clothes} alt="clothes" />
                 <div className="absolute inset-0 overflow-hidden bg-transparent  z-10  flex items-center justify-center">
                     <h1 className="text-[clamp(30px,2.5vw,4rem)] text-white font-semibold">
