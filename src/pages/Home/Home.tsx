5cf3c793bb662fe31f22903d34df94f02e6646af
yuvraj-singh08 <yuvrajsinghwahla08cs@gmail.com>
2024-05-23 10:14:08 +0530

    Added webkit to video in about

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -64,7 +64,9 @@ const Home = () => {
             src={supplychain}
             autoPlay
             muted
+            playsInline
             loop
+            webkit-playsinline
           />
           <div className="absolute w-full h-full inset-0 flex items-center justify-center backdrop-blur-md   bg-[#121e2c69]">
             <div className="flex flex-col justify-between gap-[1.5rem] max-w-screen-lg px-8">
